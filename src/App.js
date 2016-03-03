d879fb1233b1740f5a4c5c5ccd38760244ac9fc3
ykazuki <y.kazuki0614n@gmail.com>
2016-03-03 23:43:22 +0900

    up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PortalProject from './PortalProject.js'
+import PortalSlider from './PortalSlider.js'
 
 export default class App extends Component {
   constructor(props) {
@@ -7,12 +9,40 @@ export default class App extends Component {
     this.state = { message: "Reactのシンプルなコンポーネントです" }
   }
 
+  componentDidMount(){
+    this.fetchPortalData()
+  }
+
+  // async これは非同期の関数です(宣言)
+  async fetchPortalData() {
+    // Create headers
+    let headers = new Headers()
+    headers.append('Authorization', `Basic ${btoa('spring:intern_2016')}`)
+    // Call API
+    let res = await fetch("https://www.wantedlyapp.com/api/intern/portal?user_id=1468880", { headers })
+    let json = await res.json()
+    console.log(json) // 受け取った内容がコンソールに出力される
+    this.setState({
+      data: json
+    })
+  }
+
+
   render() {
     // 見た目の定義
+    let data = this.state.data
+    let popularProject = data && data.data.sections[3]
     return(
       <div>
-        <h1>Hello, World!</h1>
-        <p>{ this.state.message }</p>
+        { data ? (
+          <PortalSlider>
+            { popularProject.projects.map((project) => {
+              return <PortalProject project={project} key={project.id} />
+            }) }
+          </PortalSlider>
+        ) : (
+          <p>Now Loading...</p>
+        ) }
       </div>
     )
   }
