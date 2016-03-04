80f69bd1f33396eec0f13b4baca7e80a2414e7fa
nasa9084 <nasa9084@live.jp>
2016-03-04 13:02:36 +0900

    add data include state

--- src/RecruitCard.js
+++ src/RecruitCard.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import styles from './RecruitCard.css'
 
 export default class RecruitCard extends Component {
+    constructor(props) {
+        super(props)
+        this.orange = styles.orange + " " + styles.tagItem
+    }
 
     render() {
         let project = this.props.project
@@ -10,19 +14,19 @@ export default class RecruitCard extends Component {
                 <section className={styles.recruitCard}>
                 <header className={styles.cardHeader}>
                 <ul className={styles.tag}>
-                <li className={styles.tagItem}></li>
+                <li className={this.orange}>{project.looking_for}</li>
                 </ul>
-                <img className={styles.eyecatch}/>
+                <img className={styles.eyecatch} src={project.image.i_304_124_x2}/>
                 </header>
                 <article className={styles.cardBody}>
-                <h3 className={styles.recruitTitle}></h3>
+                <h3 className={styles.recruitTitle}>{ project.title }</h3>
                 <p className={styles.recruitMessage}>
-                lorem ipsum.
+                { `${project.description.substr(0, 70)}...` }
                 </p>
                 </article>
-                <footer className={cardFooter}>
-                <img className={styles.companyImg}/>
-                <h4 className={styles.companyName}></h4>
+                <footer className={styles.cardFooter}>
+                <img className={styles.companyImg} src={project.company.avatar.s_60} />
+                <h4 className={styles.companyName}>{project.company.name}</h4>
                 </footer>
                 </section>
         )
